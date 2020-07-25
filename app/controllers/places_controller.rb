536f059fc1c993349318e41ff75eaf03ffd94e2e
Sharlin Joseph <sharlinjoseph22@gmail.com>
2020-07-25 17:12:07 +0000

    Creating the place form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,11 @@
 class PlacesController < ApplicationController
 def index
     @places = Place.all
+    @posts = Place.order("name").page(params[:page]).per_page(5)
 end 
+
+def new
+    @place = Place.new
+end
 
 end
