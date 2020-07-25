5743e045b0c64ea75690d22913f00c546bed09d4
Sharlin Joseph <sharlinjoseph22@gmail.com>
2020-07-25 22:20:28 +0000

    connected users to places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
 def index
     @places = Place.all
     @posts = Place.order("name").page(params[:page]).per_page(5)
@@ -9,7 +10,7 @@ def new
 end
 
 def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
 end
 
