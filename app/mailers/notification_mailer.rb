78829700365b8349f1057411534c0eb67edf4dc6
Sharlin Joseph <sharlinjoseph22@gmail.com>
2020-08-25 16:19:25 +0000

    Previewing the Email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
 default from: "no-reply@nomsterapp.com"
 
     def comment_added
-        mail(to: "sharlinjoseph22@gmail.com",
+        mail(to: "debug8002@gmail.com",
             subject: "A comment has been added to your place")
     end
 end
